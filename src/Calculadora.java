d86129b32281491ee4b291e5d3176cab133e2c40
Marcelo Pitol <mrpitol@outlook.com>
2021-06-10 19:56:50 -0300

    Criadas as funcinalidades de +-*/no arquivo Calculadora

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -6,16 +6,27 @@ public class Calculadora {
 		Scanner leitor = new Scanner(System.in);
 		double valor1;
 		double valor2;
-		double soma;
-		System.out.println("Por favor digite um valor:");
-		//Não pode ser next() pois o next() é string então conflita o double 
+		double soma, divisao, multiplicacao, subtracao;
+		System.out.println("Por favor, digite um valor:");
+		//Não pode ser apenas next() pois o next() é string então conflita o double 
 		valor1 = leitor.nextDouble();
-		System.out.println("Por favor digite outro valor:");
+		System.out.println("Por favor, digite outro valor:");
 		valor2=leitor.nextDouble();
 		//Adição
 		soma = valor1+valor2;
 		System.out.println("A soma dos dois valores informados é: "+soma);
 		leitor.close();
+		//Subtração
+		subtracao=valor1-valor2;
+		System.out.println("A subtração dos dois valores informados é: "+subtracao);
+		//multiplicação
+		multiplicacao=valor1*valor2;
+		System.out.println("A multiplicação dos dois valores informados é: "+multiplicacao);
+		//divisao
+		divisao=valor1/valor2;
+		System.out.println("A divisao dos dois valores informados é: "+divisao);
+		
+		
 	}
 
 }
